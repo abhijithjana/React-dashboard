a0223bddf4482559ca6a21352ad15f60f1fdc93a
Abhijith Jana <obhijithjana@gmail.com>
2021-12-23 18:48:49 +0530

    designing almost complete

---
+++ react-dashboard/src/components/Mainmanu.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+function Mainmenu(props){
+    const [expand,setexpand]=useState(false)
+    const submenu=props.Submenu;
+   
+    return(<div>
+      
+        <li onClick={
+           props.onClick}>
+            <a className="main-menu-item">
+            <div className="mainmenu-icon">
+            <i class={props.Icon}></i>
+            </div>
+            <span onClick={()=>{setexpand(!expand)}}>{props.Name}</span></a>
+            { expand  && submenu.length > 0 ?
+              
+            <ul className={`submenu ${expand? "active" :''}`}>
+               {submenu.map((item,index)=>( <li>
+                    <a >
+                        {item.name}
+                    </a>
+                </li>))}
+               
+               
+            </ul>:""}
+           </li>
+    </div>);
+}
+export default Mainmenu;
--- react-dashboard/src/components/slide.jsx
+++ react-dashboard/src/components/slide.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assest/logo.png"
+import User from '../assest/user.png'
+import Mainmenu from "./Mainmanu";
 function Slide(){
-    return(<div className="slidmenu inactive">
+    const [togstate,setstate]=useState(false);
+  function ToggleChk(){
+    if(!togstate){
+        document.querySelectorAll('.submenu').forEach(element => {
+         element.classList.remove('active');   
+        });
+    }
+  }
+        
+  
+ 
+    const main=[{name:"Dashboard",
+    to:'/',
+    icon:"bi bi-speedometer"},
+    {
+        name:"Content",
+    to:'/content',
+    icon:"bi bi-body-text" ,
+    submenu:[{name:"projects"},{name:"Achivement"},{name:"About"}]
+
+    },{name:"Design",
+    to:'/Design',
+    icon:"bi bi-vector-pen"}];
+    
+
+    return(<div className={`slidmenu  ${togstate?'inactive':''}`}>
          <div className="top-section">
              <div className="logo">
                 <img src={Logo} alt="logo"/>
              </div>
 
              <div className="toggle" onClick={()=>{
-                 
+                 ToggleChk();
+                setstate(!togstate);
+                
+                console.log(togstate)
              }}>
              <i class="bi bi-arrow-left-square-fill"></i>
              </div>
+{togstate}
+            
             
          </div>
+
 
         <div className="search">
            
@@ -23,6 +56,39 @@ function Slide(){
             <i class="bi bi-search"></i>
             </div>
             <input type="text" placeholder="search"/>
+        </div>
+
+        <div className="divider"></div>
+
+        <div className="main-menu">
+        <ul>
+     {main.map((item,index)=>(
+       
+     
+     <Mainmenu Name={item.name}
+         Icon={item.icon}
+         Submenu={item.submenu || []}
+         state={togstate}
+         onClick={()=>{
+if(togstate){
+    setstate(false);
+}
+         }}
+     />
+     ))}
+    
+        </ul>
+     </div>
+
+        <div className="sidemenu-footer">
+            <div className="user-image">
+                <img src={User} alt="img"/>
+            </div>
+            <div className="userinfo">
+            <h5>Abhijith Jana</h5>
+            <p>abhijith.kme19cs002@gmail.com</p>
+            </div>
+            
         </div>
 
     </div>
