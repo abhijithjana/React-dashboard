f79046a0f6fd12a0008478a94cb838cf0dc974c3
Abhijith Jana <obhijithjana@gmail.com>
2021-12-23 06:02:35 +0530

    search box added and minimum mode added

--- react-dashboard/src/components/slide.jsx
+++ react-dashboard/src/components/slide.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import Logo from "../assest/logo.png"
 function Slide(){
-    return(<div className="slidmenu">
+    return(<div className="slidmenu inactive">
          <div className="top-section">
              <div className="logo">
                 <img src={Logo} alt="logo"/>
              </div>
 
-             <div className="toggle">
+             <div className="toggle" onClick={()=>{
+                 
+             }}>
              <i class="bi bi-arrow-left-square-fill"></i>
              </div>
             
          </div>
+
+        <div className="search">
+           
+               
+            
+            <div className="searchBtn">
+            <i class="bi bi-search"></i>
+            </div>
+            <input type="text" placeholder="search"/>
+        </div>
 
     </div>
     );
